8fc969feb1402714c6ac0b94b9d36d5ca276a637
Muhammad Hussain <ahsanpervez74@gmail.com>
2022-05-21 14:29:25 +0500

    added GPS

--- app/src/main/java/edu/aku/hassannaqvi/epi_register_daily/ui/LoginActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/epi_register_daily/ui/LoginActivity.java
@@ -121,7 +121,8 @@ public class LoginActivity extends AppCompatActivity {
                         Manifest.permission.WRITE_EXTERNAL_STORAGE,
                         Manifest.permission.READ_PHONE_STATE,
                         Manifest.permission.CAMERA,
-                        Manifest.permission.ACCESS_FINE_LOCATION
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
                 ).withListener(new MultiplePermissionsListener() {
             @Override
             public void onPermissionsChecked(MultiplePermissionsReport report) {
