fd6755cfae6c90fe04c5cb1814c46d9c3d031dff
Muhammad Hussain <ahsanpervez74@gmail.com>
2022-11-30 17:27:16 +0500

    *commit

--- app/src/main/java/edu/aku/hassannaqvi/epi_register_daily/core/MainApp.java
+++ app/src/main/java/edu/aku/hassannaqvi/epi_register_daily/core/MainApp.java
@@ -12,6 +12,7 @@ import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.net.ConnectivityManager;
 import android.net.Network;
@@ -265,6 +266,8 @@ public class MainApp extends Application {
 
 
         initSecure();
+
+        toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
     }
 
     private void initSecure() {
